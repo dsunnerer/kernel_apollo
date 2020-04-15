7cb075e87d453c1a5b4d91d75fc64961d86306e1
lucaswei <lucaswei@google.com>
2020-04-15 18:01:31 +0800

    sched/fair: Fix compilation issues for !CONFIG_SCHED_WALT
    For compilation issues for !CONFIG_SCHED_WALT of the following two
    commits:
    
    commit a80cf2007d94 ("sched: Add support to spread tasks")
    
    Bug: 154086870
    Bug: 153823050
    Signed-off-by: lucaswei <lucaswei@google.com>
    Change-Id: I89e224e18f6700ea2abcd162a5b9f3f938a7ad92

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3961,6 +3961,7 @@ struct find_best_target_env {
 	bool strict_max;
 };
 
+#ifdef CONFIG_SCHED_WALT
 static inline bool prefer_spread_on_idle(int cpu)
 {
 	if (likely(!sysctl_sched_prefer_spread))
@@ -3971,6 +3972,12 @@ static inline bool prefer_spread_on_idle(int cpu)
 
 	return sysctl_sched_prefer_spread > 1;
 }
+#else
+static inline bool prefer_spread_on_idle(int cpu)
+{
+	return false;
+}
+#endif
 
 static inline void adjust_cpus_for_packing(struct task_struct *p,
 			int *target_cpu, int *best_idle_cpu,
@@ -10729,10 +10736,14 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 		.loop		= 0,
 	};
 
+#ifdef CONFIG_SCHED_WALT
 	env.prefer_spread = (prefer_spread_on_idle(this_cpu) &&
 				!((sd->flags & SD_ASYM_CPUCAPACITY) &&
 				 !cpumask_test_cpu(this_cpu,
 						 &asym_cap_sibling_cpus)));
+#else
+	env.prefer_spread = false;
+#endif
 
 	cpumask_and(cpus, sched_domain_span(sd), cpu_active_mask);
 
@@ -11845,6 +11856,7 @@ static inline bool nohz_idle_balance(struct rq *this_rq, enum cpu_idle_type idle
 static inline void nohz_newidle_balance(struct rq *this_rq) { }
 #endif /* CONFIG_NO_HZ_COMMON */
 
+#ifdef CONFIG_SCHED_WALT
 static bool silver_has_big_tasks(void)
 {
 	int cpu;
@@ -11858,6 +11870,12 @@ static bool silver_has_big_tasks(void)
 
 	return false;
 }
+#else
+static inline bool silver_has_big_tasks(void)
+{
+	return false;
+}
+#endif
 
 /*
  * idle_balance is called by schedule() if this_cpu is about to become
@@ -11871,10 +11889,12 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 	int pulled_task = 0;
 	u64 curr_cost = 0;
 	u64 avg_idle = this_rq->avg_idle;
+#ifdef CONFIG_SCHED_WALT
 	bool prefer_spread = prefer_spread_on_idle(this_cpu);
 	bool force_lb = (!is_min_capacity_cpu(this_cpu) &&
 				silver_has_big_tasks() &&
 				(atomic_read(&this_rq->nr_iowait) == 0));
+#endif
 
 
 	if (cpu_isolated(this_cpu))
@@ -11927,10 +11947,12 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 		if (!(sd->flags & SD_LOAD_BALANCE))
 			continue;
 
+#ifdef CONFIG_SCHED_WALT
 		if (prefer_spread && !force_lb &&
 			(sd->flags & SD_ASYM_CPUCAPACITY) &&
 			!(cpumask_test_cpu(this_cpu, &asym_cap_sibling_cpus)))
 			avg_idle = this_rq->avg_idle;
+#endif
 
 		if (avg_idle < curr_cost + sd->max_newidle_lb_cost) {
 			update_next_balance(sd, &next_balance);
